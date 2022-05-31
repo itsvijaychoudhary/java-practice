ac23c72d521f8c5261873107ebfbd0752f1fdcf2
vijay choudhary <vijaychoudhary@vijays-MacBook-Air.local>
2022-05-31 09:32:14 +0530

    All files of java

---
+++ loops.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class loops{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1; i<11; i++){
+            System.out.println(n*i);
+        }
+    }
+}
